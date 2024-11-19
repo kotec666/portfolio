d15ba28e2bd36b550137fb975bb07049d5c0073d
kotec666 <alexkotov639@gmail.com>
2024-11-20 02:00:25 +0400

    fix img

--- src/components/pages/projectComponents/projectsData.ts
+++ src/components/pages/projectComponents/projectsData.ts
@@ -10,7 +10,7 @@ export const projectsData = [
 		projectStatus: PROJECT_STATUS.development,
 		projectDescription:"\"Quartzwerke Russia\" - это крупная компания, занимающаяся производством и продажей цветного и белого кварцевого песка оптом и в розницу. На момент написания этого текста компания имеет 4 крупных предприятия в России.",
 		href_url:"https://qw-russia.ru/",
-		img_src:"url('./images/qwr.webp')",
+		img_src:"url('/portfolio/images/qwr.webp')",
 
 	},
 	{
@@ -22,7 +22,7 @@ export const projectsData = [
 		projectStatus: PROJECT_STATUS.development,
 		projectDescription:"Мобильное приложение для отображения зарядных станций и зарядки электромобилей. Мобильное приложение реализуется на связке React + Capacitor",
 		href_url:"https://www.2chrg.com/",
-		img_src:"url('/images/2chrg.webp')"
+		img_src:"url('/portfolio/images/2chrg.webp')"
 	},
 	{
 		id: 3,
@@ -32,7 +32,7 @@ export const projectsData = [
 		projectRole: PROJECT_ROLE.commercial,
 		projectStatus: PROJECT_STATUS.finished,
 		projectDescription:"\"Bankir.kg\" - это крупный киргизский информационный портал, который специализируется на предоставлении информации о финансовых продуктах, банках, страховых компаниях, кредитах, инвестициях и других финансовых услугах. Сайт предоставляет сравнительные данные о различных банковских продуктах, а также новости и статьи о финансовой сфере. Пользователи могут находить на \"Bankir.kg\" информацию о процентных ставках по вкладам, кредитах, ипотеке, а также сравнивать различные банковские предложения. Сайт также предоставляет полезные советы и инструменты для принятия финансовых решений. Помимо этого, \"Bankir.kg\" также предоставляет информацию о страховых услугах, инвестициях, курсах валют и других финансовых темах. Этот ресурс может быть полезным и для российских граждан, ищущих информацию о финансовых продуктах и услугах, а также для принятия обоснованных решений в сфере финансов.",
-		img_src:"url('/images/bankiProject.webp')",
+		img_src:"url('/portfolio/images/bankiProject.webp')",
 	},
 ]
 
